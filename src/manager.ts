f99f0d7d943c833302c5bbe30b5f7783d80a1480
Tia <limqiying19@gmail.com>
2019-10-14 23:45:08 -0400

    remove springs

--- src/manager.ts
+++ src/manager.ts
@@ -1,7 +1,6 @@
 import Particle from "./particle";
 import { Vector3 } from "three";
 import ParticleForceRegistry from "./force-registry";
-import { Spring, createSpring } from "./spring";
 import {
   ParticleGravity,
   ParticleSpring,
@@ -10,7 +9,6 @@ import {
 
 export class ParticleManager {
   private _particles: Particle[] = [];
-  private _springs: Spring[] = [];
   private _forceRegistry: ParticleForceRegistry;
 
   constructor() {
@@ -21,29 +19,19 @@ export class ParticleManager {
     const p1 = new Particle(0, position1, velocity1, 2.0);
     this._particles.push(p1);
 
-    const position2 = new Vector3(-1.0, 0.0, 0.0);
-    const velocity2 = new Vector3(0.0, 0.0, 3.0);
+    const position2 = new Vector3(-2.0, 0.0, 0.0);
+    const velocity2 = new Vector3(0.0, 0.0, 0.0);
     const p2 = new Particle(0, position2, velocity2, 1.0);
     this._particles.push(p2);
-
-    const gravity: ParticleGravity = new ParticleGravity();
-    // this._forceRegistry.add(p1, gravity);
-    this._forceRegistry.add(p2, gravity);
 
     const k: number = 2.0;
     const ell: number = 3.0;
     const anchor: Vector3 = new Vector3(0.0, 0.0, 0.0);
-    const spring1: ParticleAnchoredSpring = new ParticleAnchoredSpring(
-      anchor,
-      k,
-      ell
-    );
-    const springObject: Spring = createSpring(p1.position, anchor);
-    this._springs.push(springObject);
-    // const spring2: ParticleSpring = new ParticleSpring(p1, k, ell);
+    const spring1: ParticleSpring = new ParticleSpring(p2, k, ell);
+    const spring2: ParticleSpring = new ParticleSpring(p1, k, ell);
 
     this._forceRegistry.add(p1, spring1);
-    // this._forceRegistry.add(p2, spring2);
+    this._forceRegistry.add(p2, spring2);
   }
 
   updateParticles(dt: number): void {
@@ -55,7 +43,4 @@ export class ParticleManager {
     return this._particles;
   }
 
-  public get springs(): Spring[] {
-    return this._springs;
-  }
 }
