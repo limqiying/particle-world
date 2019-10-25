c2f71937efd8183e3f22f46b29486fbe97c5b4b1
Tia <limqiying19@gmail.com>
2019-10-25 19:43:36 -0400

    deploy

---
+++ precache-manifest.b7eaf75d69efa4e4bc1c66afad4a9f5d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8f7a07ce936f481873cca827987d0de",
+    "url": "/particle-world/img/android-chrome-192x192.png"
+  },
+  {
+    "revision": "10f8c2647ec0f20869b61892660b87ae",
+    "url": "/particle-world/readme-images/ball-explosion.gif"
+  },
+  {
+    "revision": "71424f7183fdf7b6e72ba2416214697f",
+    "url": "/particle-world/readme-images/spring-spin.gif"
+  },
+  {
+    "revision": "9377082430df21bfa214175b31f90961",
+    "url": "/particle-world/readme-images/pool.gif"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/particle-world/robots.txt"
+  },
+  {
+    "revision": "5eae821b04dc0e3413c3dd2a3cbc5992",
+    "url": "/particle-world/readme-images/ball-hit.gif"
+  },
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/particle-world/img/site.webmanifest"
+  },
+  {
+    "revision": "8f28e6673bf2332fa8ed014f08fabc04",
+    "url": "/particle-world/img/favicon-16x16.png"
+  },
+  {
+    "revision": "e39f3e8735d61aea9273",
+    "url": "/particle-world/js/app.54dbe16d.js"
+  },
+  {
+    "revision": "b5f1bedf53a0427a39be",
+    "url": "/particle-world/js/chunk-vendors.b574e697.js"
+  },
+  {
+    "revision": "e0068b018d04f9663d4d29dcd3abd195",
+    "url": "/particle-world/img/favicon-32x32.png"
+  },
+  {
+    "revision": "801373c211724f955fb17e1e2b5ba453",
+    "url": "/particle-world/img/grass.801373c2.jpg"
+  },
+  {
+    "revision": "500e4e119822ae5879ab585449b63b2e",
+    "url": "/particle-world/index.html"
+  },
+  {
+    "revision": "d1ede15a2c4f2e643208b8caca31622f",
+    "url": "/particle-world/img/apple-touch-icon.png"
+  },
+  {
+    "revision": "5af38cfb8ffe81d8333c0f032bc4b0a5",
+    "url": "/particle-world/img/android-chrome-512x512.png"
+  },
+  {
+    "revision": "b5f1bedf53a0427a39be",
+    "url": "/particle-world/css/chunk-vendors.ceb99efa.css"
+  },
+  {
+    "revision": "e39f3e8735d61aea9273",
+    "url": "/particle-world/css/app.b86b704d.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/particle-world/precache-manifest.b7eaf75d69efa4e4bc1c66afad4a9f5d.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "engine"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
